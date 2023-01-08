059b59fbd89c707d95f423279133728c143f7f93
Rafal <rafal.malewicz1@gmail.com>
2023-01-08 13:10:04 +0000

    changes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import screenshot from "screenshot-desktop";
 import sharp from "sharp";
 import fs from "fs";
 import path from "path";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+cors({ origin: "*" });
 
 const itemy = [
   { name: "helmet", left: 1163, top: 146, width: 110, height: 70 },
@@ -10,7 +15,7 @@ const itemy = [
   { name: "boots", left: 1163, top: 537, width: 110, height: 70 },
   { name: "weapon", left: 1342, top: 537, width: 110, height: 70 },
   { name: "offhand", left: 1474, top: 537, width: 110, height: 70 },
-  { name: "slot", left: 1652, top: 537, width: 93, height: 70 },
+  { name: "slot", left: 1652, top: 537, width: 85, height: 70 },
   { name: "ring", left: 1652, top: 406, width: 110, height: 70 },
   { name: "belt", left: 1652, top: 276, width: 110, height: 70 },
   { name: "necklace", left: 1652, top: 146, width: 110, height: 70 },
@@ -19,52 +24,35 @@ const itemy = [
 const checkNewItems = (
   jsonPath: string,
   base64: string,
-  state: { newItemsCount: number }
+  onNewItemFound: Function
 ) => {
-  try {
-    const fileData = fs.readFileSync(jsonPath);
+  const fileData = fs.readFileSync(jsonPath);
 
-    const data = JSON.parse(fileData.toString());
+  const data = JSON.parse(fileData.toString());
 
-    if (!data.includes(base64)) {
-      state.newItemsCount++;
-    }
-  } catch (error) {
-    // @ts-ignore
-    if (error.errno === -4058) {
-      state.newItemsCount++;
-    }
+  if (!data.includes(base64)) {
+    onNewItemFound();
   }
 };
 
 const saveNewItems = (
   jsonPath: string,
   base64: string,
-  state: { newItemsCount: number }
+  onNewItemFound: Function
 ) => {
-  try {
-    const fileData = fs.readFileSync(jsonPath);
+  const fileData = fs.readFileSync(jsonPath);
 
-    const data = JSON.parse(fileData.toString());
+  const data = JSON.parse(fileData.toString());
 
-    if (!data.includes(base64)) {
-      data.push(base64);
-      fs.writeFileSync(jsonPath, JSON.stringify(data));
-      state.newItemsCount++;
-    }
-  } catch (error) {
-    // @ts-ignore
-    if (error.errno === -4058) {
-      fs.writeFileSync(jsonPath, JSON.stringify([base64]));
-      state.newItemsCount++;
-    }
+  if (!data.includes(base64)) {
+    data.push(base64);
+    fs.writeFileSync(jsonPath, JSON.stringify(data));
+    onNewItemFound();
   }
 };
 
-screenshot({ screen: 1 }).then(async (img: Buffer) => {
-  const state = {
-    newItemsCount: 0,
-  };
+const takeScreenshot = async (callback: Function, onNewItemFound: Function) => {
+  const img = await screenshot({ screen: 1 });
 
   for (const item of itemy) {
     const buffer = await sharp(img)
@@ -79,13 +67,36 @@ screenshot({ screen: 1 }).then(async (img: Buffer) => {
     const base64 = Buffer.from(buffer).toString("base64");
     const jsonPath = path.join(__dirname, "data", `${item.name}.json`);
 
-    if (process.argv[2] === "--check") {
-      checkNewItems(jsonPath, base64, state);
-    }
-    if (process.argv[2] === "--save") {
-      saveNewItems(jsonPath, base64, state);
+    try {
+      callback(jsonPath, base64, onNewItemFound);
+    } catch (error) {
+      // @ts-ignore
+      if (error.errno === -4058) {
+        fs.writeFileSync(jsonPath, JSON.stringify([base64]));
+        onNewItemFound();
+      }
     }
   }
+};
 
-  console.log("New items count: ", state.newItemsCount);
+app.get("/api/check", async (req, res) => {
+  let state = {
+    updatedItems: 0,
+  };
+
+  await takeScreenshot(checkNewItems, () => state.updatedItems++);
+
+  res.json(state);
 });
+
+app.get("/api/save", async (req, res) => {
+  let state = {
+    updatedItems: 0,
+  };
+
+  await takeScreenshot(saveNewItems, () => state.updatedItems++);
+
+  res.json(state);
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
