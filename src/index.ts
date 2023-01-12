f96f0e124ab5ecbd7cd41a72ef602fce0c7c3a2c
Rafal <rafal.malewicz1@gmail.com>
2023-01-12 00:07:57 +0000

    fi

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,19 @@ import cors from "cors";
 const app = express();
 cors({ origin: "*" });
 
+const overlay = fs.readFileSync(path.join(__dirname, "overlay.png"));
+
 const itemy = [
-  { name: "helmet", left: 1163, top: 146, width: 110, height: 70 },
-  { name: "armor", left: 1163, top: 276, width: 110, height: 70 },
-  { name: "gloves", left: 1163, top: 406, width: 110, height: 70 },
-  { name: "boots", left: 1163, top: 537, width: 110, height: 70 },
-  { name: "weapon", left: 1342, top: 537, width: 110, height: 70 },
-  { name: "offhand", left: 1474, top: 537, width: 110, height: 70 },
-  { name: "slot", left: 1652, top: 537, width: 85, height: 70 },
-  { name: "ring", left: 1652, top: 406, width: 110, height: 70 },
-  { name: "belt", left: 1652, top: 276, width: 110, height: 70 },
-  { name: "necklace", left: 1652, top: 146, width: 110, height: 70 },
+  { name: "helmet", left: 1163, top: 146, width: 113, height: 107 },
+  { name: "armor", left: 1163, top: 276, width: 113, height: 107 },
+  { name: "gloves", left: 1163, top: 406, width: 113, height: 107 },
+  { name: "boots", left: 1163, top: 537, width: 113, height: 107 },
+  { name: "weapon", left: 1342, top: 537, width: 113, height: 107 },
+  { name: "offhand", left: 1474, top: 537, width: 113, height: 107 },
+  { name: "slot", left: 1652, top: 537, width: 94, height: 107 },
+  { name: "ring", left: 1652, top: 406, width: 113, height: 107 },
+  { name: "belt", left: 1652, top: 276, width: 113, height: 107 },
+  { name: "necklace", left: 1652, top: 146, width: 113, height: 107 },
 ];
 
 const checkNewItems = (
@@ -26,12 +28,17 @@ const checkNewItems = (
   base64: string,
   onNewItemFound: Function
 ) => {
-  const fileData = fs.readFileSync(jsonPath);
+  try {
+    const fileData = fs.readFileSync(jsonPath);
+    const data = JSON.parse(fileData.toString());
 
-  const data = JSON.parse(fileData.toString());
-
-  if (!data.includes(base64)) {
-    onNewItemFound();
+    if (!data.includes(base64)) {
+      onNewItemFound();
+    }
+  } catch (error) {
+    if (error.errno === -4058) {
+      onNewItemFound();
+    }
   }
 };
 
@@ -40,14 +47,22 @@ const saveNewItems = (
   base64: string,
   onNewItemFound: Function
 ) => {
-  const fileData = fs.readFileSync(jsonPath);
+  try {
+    const fileData = fs.readFileSync(jsonPath);
 
-  const data = JSON.parse(fileData.toString());
+    const data = JSON.parse(fileData.toString());
 
-  if (!data.includes(base64)) {
-    data.push(base64);
-    fs.writeFileSync(jsonPath, JSON.stringify(data));
-    onNewItemFound();
+    if (!data.includes(base64)) {
+      data.push(base64);
+
+      onNewItemFound();
+      fs.writeFileSync(jsonPath, JSON.stringify(data));
+    }
+  } catch (error) {
+    if (error.errno === -4058) {
+      onNewItemFound();
+      fs.writeFileSync(jsonPath, JSON.stringify([base64]));
+    }
   }
 };
 
@@ -62,29 +77,26 @@ const takeScreenshot = async (callback: Function, onNewItemFound: Function) => {
         width: item.width,
         height: item.height,
       })
+      .composite([{ input: overlay, left: 79, top: 76 }])
       .toBuffer();
 
     const base64 = Buffer.from(buffer).toString("base64");
     const jsonPath = path.join(__dirname, "data", `${item.name}.json`);
 
-    try {
-      callback(jsonPath, base64, onNewItemFound);
-    } catch (error) {
-      // @ts-ignore
-      if (error.errno === -4058) {
-        fs.writeFileSync(jsonPath, JSON.stringify([base64]));
-        onNewItemFound();
-      }
-    }
+    callback(jsonPath, base64, () => onNewItemFound(item.name));
   }
 };
 
 app.get("/api/check", async (req, res) => {
   let state = {
     updatedItems: 0,
+    items: [] as any,
   };
 
-  await takeScreenshot(checkNewItems, () => state.updatedItems++);
+  await takeScreenshot(checkNewItems, (name: string) => {
+    state.updatedItems++;
+    state.items.push(name);
+  });
 
   res.json(state);
 });
@@ -92,9 +104,13 @@ app.get("/api/check", async (req, res) => {
 app.get("/api/save", async (req, res) => {
   let state = {
     updatedItems: 0,
+    items: [] as any,
   };
 
-  await takeScreenshot(saveNewItems, () => state.updatedItems++);
+  await takeScreenshot(saveNewItems, (name: string) => {
+    state.updatedItems++;
+    state.items.push(name);
+  });
 
   res.json(state);
 });
